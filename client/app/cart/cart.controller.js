83127606fed45110952f716c3f968811d9e93590
Sathvi <sathvisiva@users.noreply.github.com>
2017-07-26 17:04:15 +0530

    Added confirmation dialog

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,10 +1,42 @@
 'use strict';
 
 angular.module('bhcmartApp')
-.controller('CartCtrl', function($scope, Modal, ngCart, $state) {
-	$scope.clearCart = Modal.confirm.delete(function() {
-		ngCart.empty()
-	});
+.controller('CartCtrl', function($scope, Modal, ngCart, $state,$mdDialog) {
+	$scope.clearCart = function(ev) {
+	   var confirm = $mdDialog.confirm()
+          .title('Confirm')
+          .textContent('Would you like to remove all the items from your cart' )
+          .ariaLabel('remove product')
+          .targetEvent(ev)
+          .ok('Please do it!')
+          .cancel('cancel');
+
+    		$mdDialog.show(confirm).then(function() {
+      			ngCart.empty()
+    		}, function() {
+     
+    	   });
+ 		
+ 	}
+	
+	
+	
+	$scope.removeProduct = function(ev,productName , product) {
+	   var confirm = $mdDialog.confirm()
+          .title('Confirm')
+          .textContent('Would you like to remove' + productName + 'from your cart' )
+          .ariaLabel('remove product')
+          .targetEvent(ev)
+          .ok('Please do it!')
+          .cancel('cancel');
+
+    		$mdDialog.show(confirm).then(function() {
+      			ngCart.removeItemById(product.getId());
+    		}, function() {
+     
+    	   });
+ 		
+ 	}
 
 	
 
